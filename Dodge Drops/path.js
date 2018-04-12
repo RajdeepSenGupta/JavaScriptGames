894c4365613cb8057d3bfe4bbfe03f29e2de2ed2
RajdeepSenGupta <rajdeep@promactinfo.com>
2018-04-12 18:26:47 +0530

    Dodge drops javascript

---
+++ Dodge Drops/path.js
@@ -0,0 +1,12 @@
+﻿function Path() {
+    var car = new Car;
+
+    this.pathXPos = Math.round(width / 3, 2);
+    this.pathWidth = 10;
+    this.pathGap = this.pathWidth + (car.carSize * 3);
+
+    this.drawPath = function () {
+        rect(this.pathXPos, 0, this.pathWidth, height)
+        rect(this.pathXPos + this.pathGap, 0, this.pathWidth, height)
+    }
+}

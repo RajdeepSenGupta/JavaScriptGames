6448a075ac96b6de444fbc2852a3847add3a92ab
Rajdeep Sen Gupta <rajdeep@promactinfo.com>
2018-04-15 01:36:15 +0530

    Mine Sweeper javascript (#3)

---
+++ Mine Sweeper/cell.js
@@ -0,0 +1,68 @@
+﻿function Cell(i, j, cellSize) {
+    this.i = i;
+    this.j = j;
+    this.cellSize = cellSize;
+    this.cellXPos = this.j * this.cellSize;
+    this.cellYPos = this.i * this.cellSize;
+    this.mine = false;
+    this.value = 0;
+    this.hidden = true;
+    this.marked = false;
+
+    this.show = function () {
+        stroke(0);
+        noFill();
+        rect(this.cellXPos, this.cellYPos, this.cellSize, this.cellSize);
+
+        // Show mines/values
+        if (!this.hidden) {
+            if (this.mine) {
+                fill(255, 30, 30);
+                ellipse(this.cellXPos + this.cellSize / 2, this.cellYPos + this.cellSize / 2, this.cellSize / 2);
+            }
+            else {
+                textAlign(CENTER);
+                fill(0);
+                text(this.value, this.cellXPos + this.cellSize / 2, this.cellYPos + this.cellSize / 2);
+            }
+        }
+        else if (this.marked) {
+            fill(255, 30, 255);
+            ellipse(this.cellXPos + this.cellSize / 2, this.cellYPos + this.cellSize / 2, this.cellSize / 2);
+        }
+    }
+
+    this.calculateMines = function (rows, cols, grid) {
+        let mineCount = 0;
+        for (let i = this.i - 1; i <= this.i + 1; i++) {
+            for (let j = this.j - 1; j <= this.j + 1; j++) {
+                if (((i >= 0 && j >= 0) && (i < rows && j < cols)) && grid[i][j].mine) {
+                    mineCount++;
+                }
+            }
+        }
+        return mineCount;
+    }
+
+    this.containsMouse = function (mouseX, mouseY) {
+        return (mouseX >= this.cellXPos && mouseX <= this.cellXPos + this.cellSize) && (mouseY >= this.cellYPos && mouseY <= this.cellYPos + this.cellSize);
+    }
+
+    this.reveal = function () {
+        this.hidden = false;
+    }
+
+    this.flood = function (grid) {
+        for (let i = this.i - 1; i <= this.i + 1; i++) {
+            for (let j = this.j - 1; j <= this.j + 1; j++) {
+                if (((i >= 0 && j >= 0) && (i < rows && j < cols)) && !grid[i][j].mine) {
+                    grid[i][j].reveal();
+                }
+            }
+        }
+    }
+
+    this.mark = function () {
+        this.marked = !this.marked;
+    }
+}
---
+++ Mine Sweeper/main.js
@@ -0,0 +1,90 @@
+﻿let grid = [];
+let cellSize = 50;
+let rows, cols;
+let mineCount = 20;
+let isGameOver = false;
+
+function setup() {
+    var canvas = createCanvas(501, 501);
+
+    rows = Math.floor(height / cellSize);
+    cols = Math.floor(width / cellSize);
+
+    grid = createGrid(rows, cols);
+
+    // Initialize grid
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j] = new Cell(i, j, cellSize);
+        }
+    }
+
+    // Set mines
+    for (let x = 0; x < mineCount;) {
+        let i = Math.floor(random(rows));
+        let j = Math.floor(random(cols));
+
+        if (!grid[i][j].mine) {
+            grid[i][j].mine = true;
+            x++;
+        }
+    }
+
+    // Calculate mines
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j].value = grid[i][j].calculateMines(rows, cols, grid);
+        }
+    }
+}
+
+function draw() {
+    background(180);
+
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j].show();
+        }
+    }
+}
+
+function createGrid(rows, cols) {
+    let arr = new Array(rows);
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = new Array(cols);
+    }
+    return arr;
+}
+
+function mousePressed() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            if (grid[i][j].containsMouse(mouseX, mouseY) && !isGameOver) {
+                if (mouseButton === LEFT && grid[i][j].hidden) {
+                    grid[i][j].reveal();
+                    grid[i][j].flood(grid);
+
+                    if (grid[i][j].mine) {
+                        gameOver();
+                        break;
+                    }
+                }
+                else if (mouseButton === CENTER) {
+                    grid[i][j].mark();
+                }
+            }
+        }
+    }
+}
+
+function gameOver() {
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            grid[i][j].reveal();
+        }
+    }
+    setTimeout(() => {
+        isGameOver = true;
+        alert("Game Over!");
+    }, 100);
+}

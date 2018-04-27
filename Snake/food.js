0c52993fe01bc9e299903a1874ac281b90711fd8
Rajdeep Sen Gupta <rajdeep@promactinfo.com>
2018-04-27 10:35:34 +0530

    Development (#4)
    * Mine Sweeper javascript
    
    * Snake javascript

---
+++ Snake/food.js
@@ -0,0 +1,10 @@
+﻿function Food() {
+    this.foodSize = 10;
+    this.foodXPos = Math.floor(random(width - this.foodSize));
+    this.foodYPos = Math.floor(random(height - this.foodSize));
+
+    this.drawFood = function () {
+        fill(255);
+        rect(this.foodXPos, this.foodYPos, this.foodSize, this.foodSize);
+    }
+}
---
+++ Snake/main.js
@@ -0,0 +1,38 @@
+﻿let snake;
+let food;
+let score = 0;
+
+function setup() {
+    var canvas = createCanvas(800, 600);
+    frameRate(10);
+
+    food = new Food;
+    snake = new Snake;
+}
+
+function draw() {
+    background(0);
+    food.drawFood();
+    snake.drawSnake(score);
+    snake.moveSnake(score);
+
+    if (snake.eatFood(food)) {
+        score++;
+        food = new Food;
+    }
+}
+
+function keyPressed() {
+    if (keyCode === 39 && snake.xSpeed === 0) {
+        snake.redirectSnake(1, 0);
+    }
+    else if (keyCode === 40 && snake.ySpeed === 0) {
+        snake.redirectSnake(0, 1);
+    }
+    else if (keyCode === 37 && snake.xSpeed === 0) {
+        snake.redirectSnake(-1, 0);
+    }
+    else if (keyCode === 38 && snake.ySpeed === 0) {
+        snake.redirectSnake(0, -1);
+    }
+}
